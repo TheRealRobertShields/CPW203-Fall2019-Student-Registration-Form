67f145d9f568d7e5ce17d97b1e8f5c11c9b15c6a
ROBERT Shields <shields8964@students.cptc.edu>
2019-10-15 14:07:15 -0700

    Added feature to display individ students on click

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ function main() {
 function processForm() {
     var nextStu = getStudentFromForm();
     displayStudent(nextStu);
-    clearForm();
 }
 function getStudentFromForm() {
     var tempStu = new Student();
@@ -24,9 +23,31 @@ function getStudentFromForm() {
     tempStu.dateOfBirth = new Date(getInputValue("dob"));
     tempStu.address = getInputValue("address");
     tempStu.program = getInputValue("program");
-    console.log(tempStu);
+    console.log(tempStu.program);
     return tempStu;
 }
 function getInputValue(id) {
-    return document.getElementById("id").value;
+    return document.getElementById(id).value;
+}
+function displayStudent(s) {
+    var newItem = document.createElement("li");
+    newItem.innerText = s.firstName + " " + s.lastName;
+    var displaySection = document.querySelector("#student-list");
+    var list = displaySection.querySelector("ul");
+    list.appendChild(newItem);
+    console.log(newItem);
+    newItem.setAttribute("data-program", stu.program);
+    newItem.setAttribute("data-address", stu.address);
+    newItem.setAttribute("data-dateOfBirth", stu.dateOfBirth.toString());
+    newItem.onclick = showStudentData;
+}
+function showStudentData() {
+    console.log(this);
+    var currListItem = this;
+    var name = currListItem.innerText;
+    var program = currListItem.getAttribute("data-program");
+    alert(name + " is studying " + program);
+    document.querySelector("#display > h2").innerHTML = name;
+    var p = document.querySelector("#display > p");
+    p.innerHTML = name + " is studying " + program;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -36,21 +36,61 @@ function main(){
 function processForm(){
     let nextStu:Student = getStudentFromForm();
     displayStudent(nextStu);
-    clearForm();
+    //clearForm();
 }
 
 function getStudentFromForm():Student{
+    
     let tempStu = new Student();
     tempStu.firstName = getInputValue("first-name");
+    
     tempStu.lastName = getInputValue("last-name");
     tempStu.dateOfBirth = new Date(getInputValue("dob"));
     tempStu.address = getInputValue("address");
     tempStu.program = getInputValue("program");
-    console.log(tempStu);
+    console.log(tempStu.program);
     return tempStu;
 }
 
-// Helper function to extract user input from form
+// Helper function to extract user input from form.
 function getInputValue(id:string):string{
-    return (<HTMLInputElement>document.getElementById("id")).value;
+    return (<HTMLInputElement>document.getElementById(id)).value;
 }
+
+// Display functions don't return anything, void.
+function displayStudent(s:Student):void{
+    let newItem = document.createElement("li");     // Create <li>.
+    newItem.innerText = s.firstName + " " + s.lastName;
+    let displaySection = document.querySelector("#student-list");
+    let list = displaySection.querySelector("ul");
+    list.appendChild(newItem);    // Add <li> as a child to <ul>.
+    // Embed student data in <li>
+    console.log(newItem);
+    newItem.setAttribute("data-program", stu.program);
+    newItem.setAttribute("data-address", stu.address);
+    newItem.setAttribute("data-dateOfBirth", stu.dateOfBirth.toString());
+
+    // Call showStudentData when the <li> is clicked
+    newItem.onclick = showStudentData;
+
+    
+
+}
+
+function showStudentData(){
+    console.log(this);
+    let currListItem = <HTMLLIElement>this;
+    let name = currListItem.innerText;
+    let program = currListItem.getAttribute("data-program");
+    alert(name + " is studying " + program);
+    document.querySelector("#display > h2").innerHTML = name;
+    let p = document.querySelector("#display > p");
+    p.innerHTML = name + " is studying " + program;
+}
+
+
+/*
+function clearForm(){
+    
+}
+*/
